4dd974dfb109ff669cf9cb7491c3d91f6068cea1
ZhangJiachen13 <1518048969@qq.com>
2025-02-25 11:49:18 +0800

    Pizza4

--- src/Pizzeria.java
+++ src/Pizzeria.java
@@ -1,5 +1,7 @@
 public class Pizzeria {
-    
+    public final String DFF_ORDER_ID = "DEF-SOH-099";
+    public final String DEF_PIZZA_INGREDIENTS = "Mozzarella Cheese";
+    public final double DEF_ORDER_TOTAL = 15.00;
     
     public String storeAddress;
     public String storeEmail;
@@ -14,6 +16,60 @@ public class Pizzeria {
     private String drinks;
     private String orderID;
     private double orderTotal;
+
+    public Pizzeria(String DEF_ORDER_ID, String DEF_PIZZA_INGREDIENTS, double DEF_ORDER_TOTAL, String sides, String drinks){
+           orderID = DEF_ORDER_ID;
+           pizzaIngredients = DEF_PIZZA_INGREDIENTS;
+           orderTotal = DEF_ORDER_TOTAL;
+           this.sides = sides;
+           this.drinks = drinks;
+
+    }
+    public void Pizza(String order_ID, String pizza_ingredients, double order_total){
+           orderID = order_ID;
+           pizzaIngredients = pizza_ingredients;
+           orderTotal = order_total;
+
+    }
+
+    
+    public String getPizzaIngredients(){
+      return pizzaIngredients;
+    }
+    public void setPizzaIngredients(String pizzaIngredients){
+      this.pizzaIngredients = pizzaIngredients;
+    }
+    public String getPizzaPrice(){
+      return pizzaPrice;
+    }
+    public void setPizzaPrice(String pizzaPrice){
+      this.pizzaPrice = pizzaPrice;
+    }
+    public String getSides(){
+      return sides;
+    }
+    public void setSides(String sides){
+      this.sides = sides;
+    }
+    public String getDrinks(){
+      return drinks;
+    }
+    public void setDrinks(String drinks){
+      this.drinks = drinks;
+    }
+    public String getOrderID(){
+      return orderID;
+    }
+    public void setOrderID(String orderID){
+      this.orderID = orderID;
+    }
+    public double getOrderTotal(){
+      return orderTotal;
+    }
+    public void setOrderTotal(double orderTotal){
+      this.orderTotal = orderTotal;
+    }
+
 
     public void takeOrder(String id, String price,String Drinks, String Sides,String Ingredients, double total){
       
--- src/app.java
+++ src/app.java
@@ -1,8 +1,8 @@
 public class app {
 
     public static void main(String[] args) throws Exception{
-        Pizzeria pizzeria = new Pizzeria();
+        Pizzeria pizzeria = new Pizzeria("DEF-SOH-099", "Mozzarella Cheese", 15.00, "Orange juice", "6");
         
-       pizzeria.takeOrder("1234","6","Orange juice", "6","folur,Ham", 20.00);
+       pizzeria.takeOrder("DEF-SOH-099","6","Orange juice", "6","Mozzarella Cheese", 15.00);
     }
 }
