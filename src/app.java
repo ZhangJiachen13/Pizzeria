8d0e45e55d27015838935385adfb7972f6af4e75
ZhangJiachen13 <1518048969@qq.com>
2025-02-28 17:04:32 +0800

    pizza5

--- src/app.java
+++ src/app.java
@@ -2,8 +2,8 @@ public class app {
 
     public static void main(String[] args) throws Exception{
         Pizzeria pizzeria = new Pizzeria("DEF-SOH-099", "Mozzarella Cheese", 15.00, "Orange juice", "6");
-       pizzeria.processCardPayment("38465879376854");
        pizzeria.takeOrder("DEF-SOH-099","6","Orange juice", "6","Mozzarella Cheese", 15.00);
        pizzeria.special0fTheDay("Red sausage pizza", "cucumber", 3.99);
+       pizzeria.processCardPayment("38465879376854");
     }
 }
